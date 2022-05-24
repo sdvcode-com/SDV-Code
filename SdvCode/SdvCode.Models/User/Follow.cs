b93237edb6a508d708ec70bf81e052c8b2f8af22
Simeon Valev <simeonvalev00@gmail.com>
2022-05-24 15:24:02 +0300

    Work over data models

---
+++ SdvCode/SdvCode.Models/User/Follow.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="Follow.cs" company="SDV Code Data Models">
+// Copyright (c) SDV Code Data Models. All rights reserved.
+// </copyright>
+
+namespace SdvCode.Models.User
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Follow
+    {
+        public Follow()
+        {
+            this.IsFollow = true;
+        }
+
+        [Required]
+        [ForeignKey(nameof(User))]
+        public string FollowerId { get; set; }
+
+        public User Follower { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(User))]
+        public string FolloweeId { get; set; }
+
+        public User Followee { get; set; }
+
+        [Required]
+        public bool IsFollow { get; set; }
+    }
+}
